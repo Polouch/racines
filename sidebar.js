179afd022ae2e065f68a1e146c3fe52cda51b16b
Polouch <spolouchkine@orange.fr>
2026-09-03 23:17:39 +0200

    ajout d'une nouvelle rubrique "Références et articles"

--- sidebar.js
+++ sidebar.js
@@ -29,6 +29,7 @@ class SiteSidebar extends HTMLElement {
             <li><a href="nos-actions.html" class="${currentPath === 'nos-actions.html' ? 'active' : ''}">Nos actions</a></li>
             <li><a href="en-construction.html?section=videos" class="${currentPath === 'en-construction.html' && currentSearch.includes('section=videos') ? 'active' : ''}">Vidéos</a></li>
             <li><a href="en-construction.html?section=diy" class="${currentPath === 'en-construction.html' && currentSearch.includes('section=diy') ? 'active' : ''}">Le faire soi-même</a></li>
+            <li><a href="en-construction.html?section=ref" class="${currentPath === 'en-construction.html' && currentSearch.includes('section=ref') ? 'active' : ''}">Références et articles</a></li>
             <li><a href="index.html#contact">Contact</a></li>
           </ul>
         </nav>
